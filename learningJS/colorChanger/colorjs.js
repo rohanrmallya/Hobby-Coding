afe3b304c260b59f63a0b0ac962824a60b95cc70
Rohan <bangalorerohan@gmail.com>
2016-04-24 20:18:48 +0530

    Randomization of Colors and colorPicked on refresh

--- learningJS/colorChanger/colorjs.js
+++ learningJS/colorChanger/colorjs.js
@@ -1,31 +1,28 @@
-var colors = [];
-genColor();
+//var colors = ["rgb(0, 0, 255)","rgb(0, 255, 0)","rgb(255, 0, 0)",
+//			 "rgb(255, 255, 0)","rgb(0, 255, 255)","rgb(255, 0, 255)"];
+
+var colors = genColor();
 var square = document.querySelectorAll(".square");
 var picked = colorPicker();
 var colorDisp = document.querySelector("#color");
 var msg = document.querySelector("#msg");
+var neww = document.querySelector("#new");
+
 
 colorDisp.textContent = picked;
 
 
 
-for(var i=0;i<square.length;i++){
-	square[i].style.background=colors[i];
-	square[i].addEventListener("click",function(){
-		var clicked = this.style.background;
-		console.log(clicked,picked);
-		if(clicked === picked){
-			msg.textContent="Right";
-			change(picked);
 
-		}
-		else{
-			this.style.background="#252829";
-			msg.textContent="Try Again"
+neww.addEventListener("click",function(){
+	genColor();
+	picked=colorPicker();
+	for(i=0;i<square.length;i++){
+		square[i].style.background = colors[i];
+	}
+});
 
-		}
-	});
-}
+reset();
 
 function change(col){
 	for(var i=0; i<square.length; i++){
@@ -45,9 +42,32 @@ function gen(){
 }
 
 function genColor(){
+	var colors = [];
 	for(i=0; i<7;i++){
+	
 	var col = String(("rgb("+String(gen())+", "+String(gen())+", "+String(gen())+")"));
 	colors.push(col);
 	}
+	return colors;
 	console.log(colors);
+}
+
+function reset(){
+	for(var i=0;i<square.length;i++){
+	square[i].style.background=colors[i];
+	square[i].addEventListener("click",function(){
+		var clicked = this.style.background;
+		console.log(clicked,picked);
+		if(clicked === picked){
+			msg.textContent="Right";
+			change(picked);
+
+		}
+		else{
+			this.style.background="#252829";
+			msg.textContent="Try Again"
+
+		}
+	});
+}
 }
