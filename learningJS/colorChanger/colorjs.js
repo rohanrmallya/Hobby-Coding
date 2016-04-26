17589c924fb60022a78db4e9dbd0341c5156a3ce
Rohan <bangalorerohan@gmail.com>
2016-04-26 10:17:29 +0530

    Added : Easy and Hard Buttons
    Easy buttons lets you play with only 3 squares, Hard button lets you play with 6 squares.

--- learningJS/colorChanger/colorjs.js
+++ learningJS/colorChanger/colorjs.js
@@ -1,37 +1,83 @@
 //var colors = ["rgb(0, 0, 255)","rgb(0, 255, 0)","rgb(255, 0, 0)",
 //			 "rgb(255, 255, 0)","rgb(0, 255, 255)","rgb(255, 0, 255)"];
 
-var colors = genColor();
+var colors = genColor(6);
 var square = document.querySelectorAll(".square");
 var picked = colorPicker();
 var colorDisp = document.querySelector("#color");
 var msg = document.querySelector("#msg");
 var neww = document.querySelector("#new");
+var easy = document.querySelector('#easy');
+var hard = document.querySelector('#hard');
+
+for(i=0;i<square.length;i++){
+	square[i].style.opacity = 1;
+}
+
+easy.addEventListener("click",function(){
+	colors = genColor(3);
+	picked = colorPicker();
+	for(i=0;i<square.length;i++){
+		square[i].style.background = colors[i];
+	};
+	for(i=3;i<6;i++){
+		square[i].style.opacity = 0;
+	}
+	colorDisp.textContent = picked;
+});
+
+hard.addEventListener("click",function(){
+	colors = genColor(6);
+	picked = colorPicker();
+	for(i=0;i<square.length;i++){
+		square[i].style.background = colors[i];
+	};
+	for(i=3;i<6;i++){
+		square[i].style.opacity = 1;
+	};
+	colorDisp.textContent = picked;
+	
+});
 
 
 colorDisp.textContent = picked;
 
-
-console.log(colors);
-
 neww.addEventListener("click",function(){
-	colors = genColor();
-	picked = colorPicker();
-	for(i=0;i<square.length;i++){
-		square[i].style.background = colors[i];
+	if(square[3].style.opacity == 0){
+		colors = genColor(3);
+		picked = colorPicker();
+		for(i=0;i<square.length;i++){
+			square[i].style.background = colors[i];
+		};
+		for(i=3;i<6;i++){
+			square[i].style.opactiy = 0;
+		};
+		colorDisp.textContent = picked;
 	}
-	console.log(colors);
+	else{
+		colors = genColor(6);
+		picked = colorPicker();
+		for(i=0;i<square.length;i++){
+			square[i].style.background = colors[i];
+		};
+		for(i=3;i<6;i++){
+			square[i].style.opactiy = 1;
+		};
+		colorDisp.textContent = picked;
+
+	}
+	msg.textContent = null;
+	
 	
 });
 
-console.log(colors);
+
 
 for(var i=0;i<square.length;i++){
 
 	square[i].style.background=colors[i];
 	square[i].addEventListener("click",function(){
 		var clicked = this.style.background;
-		console.log(clicked,picked);
 		if(clicked === picked){
 			msg.textContent="Right";
 			change(picked);
@@ -62,14 +108,14 @@ function gen(){
 	return num;
 }
 
-function genColor(){
+function genColor(size){
 	var arr = [];
-	for(i=0; i<7;i++){
+	for(i=0; i<size;i++){
 	
 	var col = String(("rgb("+String(gen())+", "+String(gen())+", "+String(gen())+")"));
 	arr.push(col);
 	}
 	return arr;
-	console.log(colors);
+	
 }
 
