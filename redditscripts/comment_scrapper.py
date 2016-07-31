a63c9446da8d37a19f56a8ec1fc95b4354f1153f
Rohan Mallya <bangalorerohan@gmail.com>
2016-07-31 21:19:37 +0530

    Added License

--- redditscripts/comment_scrapper.py
+++ redditscripts/comment_scrapper.py
@@ -1,5 +1,21 @@
 ''' Comment Scrapper scrapes through all the comments for a particular user /u/user and stores them in a user.txt '''
 
+''' 
+Copyright (c) 2016 Rohan Mallya
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
 
 import praw
 
